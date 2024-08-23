0b8676489e63de74b7b21d0d9fb8ac385c331216
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-23 23:55:15 +0300

    add file lib to gpio

---
+++ src/lib/gpio_uno.cpp
@@ -0,0 +1,11 @@
+//
+// File: gpio_uno.cpp
+//
+// this file contain the implementation of the header
+// file gpio_uno.h define the set_mode & set_state
+// functions for each pin.
+//
+
+#include "gpio_uno.h"
+
+// code here
