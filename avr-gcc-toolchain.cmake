bfbbbc9940cf8c2bebaa80a572d17a5de7af64d3
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-24 23:24:24 +0300

    add new file for compile and upload the iHex file to Arduino Board

---
+++ avr-gcc-toolchain.cmake
@@ -0,0 +1,16 @@
+# Define the toolchain for AVR
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
+
+# Set the compiler and tools
+set(CMAKE_C_COMPILER avr-gcc)
+set(CMAKE_CXX_COMPILER avr-g++)
+
+# Specify the path to the AVR toolchain, if not in PATH
+# set(AVR_TOOLCHAIN_PATH /path/to/avr-toolchain)
+# set(CMAKE_C_COMPILER ${AVR_TOOLCHAIN_PATH}/bin/avr-gcc)
+# set(CMAKE_CXX_COMPILER ${AVR_TOOLCHAIN_PATH}/bin/avr-g++)
+
+# Define the CMake flags for AVR
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmcu=atmega328p -DF_CPU=16000000UL")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmcu=atmega328p -DF_CPU=16000000UL")
