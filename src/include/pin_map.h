93721230ad4502a4b114f4be71d38007bf011961
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-28 16:53:41 +0300

    add the digital pin 0 -7, (D) bank map

--- src/include/pin_map.h
+++ src/include/pin_map.h
@@ -25,4 +25,20 @@ enum Digital_Pin_B: uint8_t {
   PIN8 = 0
 };
 
+/**
+ *
+ * @brief: this enumeration contains the digital GPIO pins maps from (0 - 7)
+ *         its create a pin maps for digital pin (D) with (8bit) integer type.
+ *
+ **/
+enum Digital_Pin_D: uint8_t {
+  PIN7 = 7,
+  PIN6 = 6,
+  PIN5 = 5,
+  PIN4 = 4,
+  PIN3 = 3,
+  PIN2 = 2,
+  PIN1 = 1,
+  PIN0 = 0
+};
 #endif     // _PIN_MAP_H_
