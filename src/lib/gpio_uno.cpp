03f48f53bb01e8031085f252b47e2d254df56d47
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-29 00:08:05 +0300

    add new function

--- src/lib/gpio_uno.cpp
+++ src/lib/gpio_uno.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "gpio_uno.h"
+#include "pin_map.h"
+
 #include <stdint.h>
 
 // <<<<<<<<<<====================>>>>>>>>>>(pin_set_mode()) this function set the pin mode (direction) that mean (output/input).
@@ -30,4 +32,20 @@ bool pull_check(const unsigned int& pin_number)
   if(!(PINB&(1<<pin_number)))
     return false;
   return true;
+}
+
+// <<<<<<<<<<====================>>>>>>>>>>(port_form_pin) this function return the port (b,c,d) depend on pin number.
+void port_from_pin(volatile unsigned char* gpio_port, uint8_t pin_number)
+{
+  switch (pin_number)
+    {
+    case PIN13:
+    case PIN12:
+    case PIN11:
+    case PIN10:
+    case PIN9:
+    case PIN8:
+      *gpio_port = DDRB;
+      break;
+    }
 }
