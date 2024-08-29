d9aecc3d4a76d1d98196387bd01a530925d4771c
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-29 16:18:02 +0300

    update to use the port_from_pin function

--- src/lib/gpio_uno.cpp
+++ src/lib/gpio_uno.cpp
@@ -12,10 +12,12 @@
 #include <stdint.h>
 
 // <<<<<<<<<<====================>>>>>>>>>>(pin_set_mode()) this function set the pin mode (direction) that mean (output/input).
-void pin_set_mode(unsigned int pin, pin_mode mode)
+void pin_set_mode(digital_pin pin, pin_mode mode)
 {
-  if(port_from_pin(pin) == 'B')
-    DDRB |= (mode << static_cast<uint8_t>(pin));
+  port_bank port;
+  port_from_pin(port, pin);
+  port.DDRx |= (mode << port.bit);
+  // DDRB |= (mode << static_cast<uint8_t>(pin));
 }
 
 // <<<<<<<<<<====================>>>>>>>>>>(pin_set_state()) this function set the pin state (voltage) that mean (high/low).
@@ -38,30 +40,18 @@ bool pull_check(const unsigned int& pin_number)
 // <<<<<<<<<<====================>>>>>>>>>>(port_form_pin) this function select the port (b,c,d) depend on pin number.
 void port_from_pin(port_bank& port, digital_pin pin)
 {
-  switch (pin) {
-  case PIN13:
-  case PIN12:
-  case PIN11:
-  case PIN10:
-  case PIN9:
-  case PIN8:
-    port.PORTx = &PORTB;
-    port.DDRx = &DDRB;
-    port.PINx = &PINB;
-    port.bit = static_cast<uint8_t>(pin)-8;
-    break;
-  case PIN7:
-  case PIN6:
-  case PIN5:
-  case PIN4:
-  case PIN3:
-  case PIN2:
-  case PIN1:
-  case PIN0:
-    port.PORTx = &PORTD;
-    port.DDRx = &DDRD;
-    port.PINx = &PIND;
-    port.bit = static_cast<uint8_t>(pin);
-    break;
-      }
+  if(static_cast<uint8_t>(pin) >= 8)
+    {
+      port.PORTx = &PORTB;
+      port.DDRx = &DDRB;
+      port.PINx = &PINB;
+      port.bit = static_cast<uint8_t>(pin)-8;
+    }
+  else
+    {
+      port.PORTx = &PORTD;
+      port.DDRx = &DDRD;
+      port.PINx = &PIND;
+      port.bit = static_cast<uint8_t>(pin);
+    }
 }
