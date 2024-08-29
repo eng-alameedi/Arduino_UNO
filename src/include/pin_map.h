45f647314d93274ccc446aa7c7e1f6295fb5baab
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-29 15:38:13 +0300

    add struct to define port number and it's bits.

--- src/include/pin_map.h
+++ src/include/pin_map.h
@@ -12,33 +12,39 @@
 
 /**
  *
- * @brief: this enumeration contains the digital GPIO pins maps form (8 - 13).
- *         its create a pin maps for digital pin (B) with (8bit) integer type.
+ * @brief: this enumeration contains the digital GPIO pins maps form (0 - 13).
+ *         its create a pin maps for digital pin (B, and D) to each bit in (8bit) integer type port.
  *
  **/
-enum Digital_Pin_B: uint8_t {
+enum digital_pin: uint8_t {
   PIN13 = 5,
   PIN12 = 4,
   PIN11 = 3,
   PIN10 = 2,
   PIN9 = 1,
-  PIN8 = 0
-};
-
-/**
- *
- * @brief: this enumeration contains the digital GPIO pins maps from (0 - 7)
- *         its create a pin maps for digital pin (D) with (8bit) integer type.
- *
- **/
-enum Digital_Pin_D: uint8_t {
+  PIN8 = 0,
   PIN7 = 7,
   PIN6 = 6,
   PIN5 = 5,
   PIN4 = 4,
   PIN3 = 3,
   PIN2 = 2,
-  PIN1 = 1,
+  PIN1 =1,
   PIN0 = 0
 };
+
+/**
+ *
+ * @brief: define a struct type for hold the port bank like (DDRx, PORTx, PINx)
+ *
+ *
+ *
+ *
+ **/
+struct port_bank {
+  volatile uint8_t* DDRx;
+  volatile uint8_t* PORTx;
+  volatile uint8_t* PINx;
+  uint8_t bit;
+}
 #endif     // _PIN_MAP_H_
