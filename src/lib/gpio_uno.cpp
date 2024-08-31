eafba87d0ef43a2fb98f1d17df78e3e1c8fa2d11
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-31 23:28:45 +0300

    update the function set_pin_mode()

--- src/lib/gpio_uno.cpp
+++ src/lib/gpio_uno.cpp
@@ -5,7 +5,7 @@
 // file gpio_uno.h define the set_mode & set_state
 // functions for each pin.
 //
-/*
+
 #include "gpio_uno.h"
 
 #include <stdint.h>
@@ -13,15 +13,21 @@
 #include "pin_map.h"
 
 // <<<<<<<<<<====================>>>>>>>>>>(pin_set_mode()) this function set
-the pin mode (direction) that mean (output/input). void pin_set_mode(digital_pin
-pin, pin_mode mode)
-{
-  port_bank port;
-  port_from_pin(port, pin);
-  *(port.DDRx) |= (mode << port.bit);
-  // DDRB |= (mode << static_cast<uint8_t>(pin));
+// the pin mode (direction) that mean (output/input).
+bool pin_set_mode(digital_pin pin, pin_mode mode) {
+  uint8_t bi;
+  if (static_cast<uint8_t>(pin) >= 8) {
+    bi = static_cast<uint8_t>(pin) - 8;
+    DDRB |= (mode << bi);
+    return true;
+  } else {
+    bi = static_cast<uint8_t>(pin);
+    DDRC |= (mode << bi);
+    return true;
+  }
+  return false;
 }
-
+/*
 // <<<<<<<<<<====================>>>>>>>>>>(pin_set_state()) this function set
 the pin state (voltage) that mean (high/low). void pin_set_state(digital_pin
 pin, pin_state state)
