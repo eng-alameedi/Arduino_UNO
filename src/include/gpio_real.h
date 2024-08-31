69a24306b82f9aa88a877246992183c8aad4aff1
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-31 16:21:48 +0300

    add gpio_real class that implement the real microchip

---
+++ src/include/gpio_real.h
@@ -0,0 +1,19 @@
+//
+// File: gpio_real.h
+//
+// this file contain the class gpio_real that,
+// inherited from gpio class so simulate the,
+// real ATmega328p microchip
+//
+
+#ifndef _GPIO_REAL_H_
+#define _GPIO_REAL_H_
+
+#include "gpio_init.h"
+
+class GPIO_REAL : public GPIO {
+ protected:
+  void set_pinmode(digital_pin, pin_mode) override;
+};
+
+#endif  // _GPIO_REAL_H_
