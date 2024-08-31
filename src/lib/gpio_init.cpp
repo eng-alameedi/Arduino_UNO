1d708b12638f761ff4e30b4ed3a6c149f2fc300d
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-08-31 21:26:32 +0300

    update

--- src/lib/gpio_init.cpp
+++ src/lib/gpio_init.cpp
@@ -6,15 +6,16 @@
 //
 
 #include "gpio_init.h"
+
 #include "gpio_uno.h"
 #include "pin_map.h"
 
-GPIO::GPIO(digital_pin pin, pin_mode mode): pin{pin},mode{mode}
-{
+GPIO::GPIO(digital_pin pin, pin_mode mode) : pin{pin}, mode{mode} {
   pin_set_mode(pin, mode);
 }
 
-GPIO::~GPIO()
+/*GPIO::~GPIO()
 {
   // code here
 }
+*/
