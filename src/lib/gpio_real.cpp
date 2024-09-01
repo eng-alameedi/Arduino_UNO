b0d1e3a70773285e70c850f35a6c46d988d8d057
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-01 22:35:45 +0300

    update the set_pinmode()

--- src/lib/gpio_real.cpp
+++ src/lib/gpio_real.cpp
@@ -12,9 +12,7 @@
 #include "gpio_uno.h"
 #include "pin_map.h"
 
-GPIO_REAL::GPIO_REAL(digital_pin p, pin_mode m) : GPIO(p, m) {
-  set_pinmode(p, m);
-}
+GPIO_REAL::GPIO_REAL(digital_pin p, pin_mode m) : GPIO(p, m) { set_pinmode(); }
 
 GPIO_REAL::~GPIO_REAL() {
   // code here
