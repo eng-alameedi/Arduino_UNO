5b8d0e71932f2200e434bf6a2babb2b4749e27f5
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-03 23:32:41 +0300

    add new test

--- tests/test_gpio.cpp
+++ tests/test_gpio.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "Arduino_Uno.h"
+#include "pin_map.h"
 
 // Test case for GPIO pin initialization
 TEST(GPIOTest, Initialization) {
@@ -12,9 +13,9 @@ TEST(GPIOTest, Initialization) {
 // Test case for setting and getting pin state
 TEST(GPIOTest, SetGetState) {
   GPIO_REAL gpio(PIN13, OUTPUT);
-  // ASSERT_EQ(gpio.get_state(), HIGH);
-  // gpio.set_state(LOW);
   ASSERT_EQ(gpio.get_state(), LOW);
+  gpio.set_pinstate(HIGH);
+  ASSERT_EQ(gpio.get_state(), HIGH);
 }
 TEST(GPIOTest, VarTest) {
   ASSERT_EQ(static_cast<uint8_t>(PIN13), 13);
@@ -24,6 +25,9 @@ TEST(GPIOTest, VarTest) {
 TEST(GPIOTest, PortTest) {
   GPIO_REAL gpio(PIN13, OUTPUT);
   ASSERT_EQ(static_cast<int>(*(gpio.get_ddrx())), 32);
+  ASSERT_EQ(static_cast<int>(*(gpio.get_portx())), 0);
+  gpio.set_pinstate(HIGH);
+  ASSERT_EQ(static_cast<int>(*(gpio.get_portx())), 32);
 }
 
 int main(int argc, char **argv) {
