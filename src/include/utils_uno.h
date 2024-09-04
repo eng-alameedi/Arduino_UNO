edc5c7b8b17a285d5dfa73f73392a09f5cabb9a4
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-05 00:27:34 +0300

    update the PIN_INIT macro to use GPIO_REAL class

--- src/include/utils_uno.h
+++ src/include/utils_uno.h
@@ -9,8 +9,14 @@
 #ifndef _UTILS_UNO_H_
 #define _UTILS_UNO_H_
 
-#define PIN_MODE(number, mode)       pin_set_mode(number, mode) // define a macro to call the function set_pin_mode
-#define PIN_STATE(number, state)     pin_set_state(number,state) // define a macro to call the function set_pin_state
-#define PIN_INIT(number, mode)       GPIO gpio_##number(number,mode) // define a macro to initial the class GPIO
+#define PIN_MODE(number, mode) \
+  pin_set_mode(number,         \
+               mode)  // define a macro to call the function set_pin_mode
+#define PIN_STATE(number, state) \
+  pin_set_state(number,          \
+                state)  // define a macro to call the function set_pin_state
+#define PIN_INIT(number, mode)  \
+  GPIO_REAL gp_##number(number, \
+                        mode)  // define a macro to initial the class GPIO
 
-#endif     // _UTILS_UNO_H_
+#endif  // _UTILS_UNO_H_
