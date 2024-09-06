49061d456b429c197e32bf1230954c747779cde7
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-06 16:40:45 +0300

    create mock class for test the gpio

---
+++ src/include/gpio_mock.h
@@ -0,0 +1,24 @@
+//
+// File: gpio_mock.h
+//
+// this file contain the implementation of the gpio
+// class for mocking purpose, and testing.
+//
+
+#ifndef _GPIO_MOCK_H_
+#define _GPIO_MOCK_H_
+
+#include <gmock/gmock.h>
+
+#include "gmock/gmock.h"
+#include "gpio_init.h"
+#include "pin_map.h"
+
+class GPIO_MOCK : public GPIO {
+ public:
+  MOCK_METHOD(void, set_pinmode, (), (override));
+  MOCK_METHOD(void, set_pinstate, (pin_state), (override));
+  MOCK_METHOD(bool, digital_pinread, (), (override));
+};
+
+#endif  // _GPIO_MOCK_H_
