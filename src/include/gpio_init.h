62d24d62233f3ac985084b9dfed3cb650ef9de5e
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-07 21:32:41 +0300

    update the type of DDRx, PORTx, PINx to uint8_t

--- src/include/gpio_init.h
+++ src/include/gpio_init.h
@@ -9,32 +9,35 @@
 #ifndef _GPIO_INIT_H_
 #define _GPIO_INIT_H_
 
+#include <stdint.h>
+
 #include "pin_map.h"
 
 class GPIO {
  protected:
-  volatile unsigned char* DDRx;   // pointer that hold the address of DDR(B,C,D)
-                                  // depend on pin number.
-  volatile unsigned char* PORTx;  // pointer that hold the address of
-                                  // PORT(B,C,D) depend on pin number.
-  volatile unsigned char* PINx;   // pointer that hold the address of PIN(B,C,D)
-                                  // depend on pin number.
-  uint8_t bit;                    // the correct bit of the pin selected.
-  digital_pin pin;                // enum define the pin number map.
-  pin_mode mode;                  // enum pin mode (OUT,IN).
-  pin_state state;                // enum pin state (HIGH,LOW).
-  int error_count{};  // error count when function not work properly.
+  volatile uint8_t* DDRx;   // pointer that hold the address of DDR(B,C,D)
+                            // depend on pin number.
+  volatile uint8_t* PORTx;  // pointer that hold the address of
+                            // PORT(B,C,D) depend on pin number.
+  volatile uint8_t* PINx;   // pointer that hold the address of PIN(B,C,D)
+                            // depend on pin number.
+  uint8_t bit{};            // the correct bit of the pin selected.
+  digital_pin pin;          // enum define the pin number map.
+  pin_mode mode{INPUT};     // enum pin mode (OUT,IN).
+  pin_state state{LOW};     // enum pin state (HIGH,LOW).
+  int error_count{};        // error count when function not work properly.
 
  public:
   GPIO(digital_pin, pin_mode);  // GPIO class constructor.
   virtual ~GPIO() = default;    // GPIO class destructor.
+
   digital_pin get_pin() const;
   pin_mode get_mode() const;
   pin_state get_state() const;
   uint8_t get_bit() const;
 
-  unsigned char get_ddrx() const;
-  unsigned char get_portx() const;
+  uint8_t get_ddrx() const;
+  uint8_t get_portx() const;
 
   virtual void set_pinstate(pin_state) = 0;
 
