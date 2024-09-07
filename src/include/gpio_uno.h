2ddc6ecbb5ed8f9cde34f14e6389b7318f2e96d8
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-07 21:23:33 +0300

    update the register type from char* to uint8_t*

--- src/include/gpio_uno.h
+++ src/include/gpio_uno.h
@@ -3,67 +3,52 @@
 //
 // this file contain definition of the arduino uno,
 // gpio ports type, mode, and state for all the pin.
+// group B,C,D for digital pin from (0 - 13), and
+// analog pin from (1 - 6).
 //
 
 #ifndef _GPIO_UNO_H_
 #define _GPIO_UNO_H_
 
-// #include "pin_map.h"
+#include <stdlib.h>
 
-#define PORTB *((volatile unsigned char*)0x25)  // gpio (B) group data register.
-#define DDRB \
-  *((volatile unsigned char*)0x24)  // gpio (B) group data direction register.
-#define PINB \
-  *((volatile unsigned char*)0x23)  // gpio (B) group input pins address.
+/**
+ * @brief: this block of definition for the block (B) of ATmega328p register.
+ *
+ * @PORTB: this is the port (b) register which set the bit to high or low.
+ *
+ * @DDRB: this is the ddr (b) register which set the bit to input or output.
+ *
+ * @PINB: this the pin (b) register which read the input digital signal to pin.
+ **/
+#define PORTB *((volatile uint8_t*)0x25)
+#define DDRB *((volatile uint8_t*)0x24)
+#define PINB *((volatile uint8_t*)0x23)
 
-#define PORTC *((volatile unsigned char*)0x28)  // gpio (C) group data register.
-#define DDRC \
-  *((volatile unsigned char*)0x27)  // gpio (C) group data direction register.
-#define PINC \
-  *((volatile unsigned char*)0x26)  // gpio (C) group input pins address.
+/**
+ * @brief: this block of definition for the block (C) of ATmega328p register.
+ *
+ * @PORTB: this is the port (c) register which set the bit to high or low.
+ *
+ * @DDRB: this is the ddr (c) register which set the bit to input or output.
+ *
+ * @PINB: this the pin (c) register which read the input digital signal to pin.
+ **/
+#define PORTC *((volatile uint8_t*)0x28)
+#define DDRC *((volatile uint8_t*)0x27)
+#define PINC *((volatile uint8_t*)0x26)
 
-#define PORTD *((volatile unsigned char*)0x2B)  // gpio (D) group data register.
-#define DDRD \
-  *((volatile unsigned char*)0x2A)  // gpio (D) group data direction register.
-#define PIND \
-  *((volatile unsigned char*)0x29)  // gpio (D) group input pins address.
+/**
+ * @brief: this block of definition for the block (B) of ATmega328p register.
+ *
+ * @PORTB: this is the port (d) register which set the bit to high or low.
+ *
+ * @DDRB: this is the ddr (d) register which set the bit to input or output.
+ *
+ * @PINB: this the pin (d) register which read the input digital signal to pin.
+ **/
+#define PORTD *((volatile uint8_t*)0x2B)
+#define DDRD *((volatile uint8_t*)0x2A)
+#define PIND *((volatile uint8_t*)0x29)
 
-/*
- *
- * @brief: this function set the pin mode (output) or (input) depend on pin
- *number.
- *
- * @param1: this function take (uint8_t) for pin number.
- *
- * @param2: this function take (pin_mode) for mode (output/input).
- *
- * @return: this function return nothing (void).
- *
- */
-// bool pin_set_mode(volatile unsigned char*, pin_mode, uint8_t b);
-
-/*
- *
- * @brief: this function set the pin state to (low) or (high) voltage.
- *
- * @param1: this function take (uint8_t) for pin number.
- *
- * @param2: this function take (pin_state) for set the pin (high/low)
- *
- * @return: this function return nothing (void)
- *
- */
-// bool pin_set_state(volatile unsigned char*, pin_state, uint8_t);
-
-/*
- *
- * @brief: this function check the pin input, after set the pin direction to
- *         input, and pull-up resistor to on.
- *
- * @param1: this function take a (unsigned int) pin number.
- *
- * @return: this function return (true) or (false) depend on input pin state.
- *
- */
-// bool pull_check(digital_pin);
 #endif  // _GPIO_UNO_H_
