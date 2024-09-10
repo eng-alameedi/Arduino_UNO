faa1a578c44f98e603d84262059a5c8a60d23765
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-10 15:28:10 +0300

    header file update contain

--- src/include/gpio_real.h
+++ src/include/gpio_real.h
@@ -17,15 +17,14 @@ class GPIO_REAL : public GPIO {
   GPIO_REAL(digital_pin, pin_mode);
   // ~GPIO_REAL();
 
-  void set_pinstate(pin_state) override;
-  bool digital_pinread() override;
+  void set_pinstate(pin_state) override;  // set pin state overridden method
 
-  static void SignalHandler(int);
+  bool digital_pinread() override;  // digital pin read overridden method
+
+  static void SignalHandler(int);  // this method for OS signal handler
 
  protected:
-  void set_pinmode() override;
-  // void set_pinstate(pin_state) override;
-  // bool digital_pinread() override;
+  void set_pinmode() override;  // set pin mode overridden method
 };
 
 #endif  // _GPIO_REAL_H_
