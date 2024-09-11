686f00ab82012ae8eee688fe0e7c23fa0576bdbb
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-12 00:27:33 +0300

    add count() function and increase counter by 1

--- src/lib/timers.cpp
+++ src/lib/timers.cpp
@@ -12,24 +12,37 @@
 
 void sei() {
   SREG |= (1 << 7);  // enable the global interrupts
+  // same as __asm__ __volatile__("sei");
 }
 
 void cli() {
   SREG &= ~(1 << 7);  // disable the global interrupts
+  // same as __asm__ __volatile__("cli");
 }
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<================================>>>>>>>>>>>>>>>>>>>>>>>>>>
 Timer0::Timer0() : Timer() {}
 
 void Timer0::count_setup() {
-  __asm__ __volatile__("cli");  // disable the global interrupts during the timer configuration.
+  cli();  // disable the global interrupts during the timer configuration.
+
   TCCR0A = 0;
   TCCR0B = 0;
+  TCNT0 = 0;
 
   TCCR0B |= (HIGH << CS02) | (HIGH << CS00);  // set the prescaler to 1024 101
-  TCNT0 = 0;
-  __asm__ __volatile__("sei");  // enable the global interrupts after completing the timer configuration.
+  TIMSK0 |= (HIGH << TOIE0);                  // enable timer overflow interrupt
+
+  sei();  // enable the global interrupts after completing the timer configuration.
 }
 
 void Timer0::count() {
-  // code here
+  bool loop_condition{true};
+  while (loop_condition) {
+    if (TIFR0 & (1 << TOV0)) {
+      counter++;
+      TIFR0 |= (HIGH << TOV0);
+      loop_condition = false;
+    }
+  }
 }
