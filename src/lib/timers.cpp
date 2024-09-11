83e7825bc85a07d7725b0b229285d89848e2d361
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-09-11 23:44:50 +0300

    add some functionality to the file

--- src/lib/timers.cpp
+++ src/lib/timers.cpp
@@ -13,12 +13,15 @@
 Timer0::Timer0() : Timer() {}
 
 void Timer0::count_setup() {
+  __asm__ __volatile__("cli");  // disable the global interrupts during the timer configuration.
   TCCR0A = 0;
   TCCR0B = 0;
 
-  TCCR0B |= (HIGH <<)
+  TCCR0B |= (HIGH << CS02) | (HIGH << CS00);  // set the prescaler to 1024 101
+  TCNT0 = 0;
+  __asm__ __volatile__("sei");  // enable the global interrupts after completing the timer configuration.
+}
 
-      void
-      Timer0::count() {
-    // code here
-  }
+void Timer0::count() {
+  // code here
+}
