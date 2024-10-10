c78f1e40e4324c7b718abf8dd76a9e43560dcfe6
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2024-10-10 22:09:48 +0300

    add new example for USART

---
+++ examples/usart_test/setup.cpp
@@ -0,0 +1,34 @@
+//
+// File: setup.cpp
+//
+// this file will contain the user example led-button toggle
+// that test and implement the GPIO_REAL.
+//
+
+#include "Arduino_test.h"
+#include "serial_uno.h"
+
+const char* msg = "Hello From Arduino Uno \n\r";
+
+PIN_INIT(PIN13, OUTPUT);
+PIN_INIT(PIN8, OUTPUT);
+PIN_INIT(PIN2, INPUT);
+
+void setup() {
+  PIN_STATE(PIN8, LOW);
+  PIN_STATE(PIN2, HIGH);
+  usart_init();
+}
+
+void loop() {
+  PIN_STATE(PIN13, HIGH);
+  DELAY(1000);
+  PIN_STATE(PIN13, LOW);
+  DELAY(1000);
+  if (PIN_READ(PIN2)) {
+    while (*msg) {
+      usart_transmit(*msg++);
+    }
+    PIN_STATE(PIN8, HIGH);
+  }
+}
